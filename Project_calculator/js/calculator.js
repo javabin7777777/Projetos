cac049715d39d8045da210c8ae4a7a33d6a41bbe
javabin7777777 <fisica14@hotmail.com>
2023-03-12 19:29:19 -0300

    update calculator.js(power2)

--- Project_calculator/js/calculator.js
+++ Project_calculator/js/calculator.js
@@ -1,20 +1,27 @@
 
 "use strict";
-
 /* inicialização de variáveis */
 
+//var control=false; // valor default
+//var aux;
+//var tempo;
+//var equal=true;
 const DECIMAL=10;
+//const array=[];
 const A=[];
-const input = document.querySelector("#input");
-const div= document.querySelector("#containerButton");
+//const  text = "not a number!";
+//var zero = um =0;
+const input = document.querySelector("#input");//campo texto .
+const div= document.querySelector("#containerButton");// div dos 'buttons' .
 input.value="";
+//console.log(div);
 
 //criar os 'buttons' de '0 a 9'
 for (let i = 0; i < 10; i++) {
     div.innerHTML = div.innerHTML + `<button class="number">${i}</button>`;
 }
 
-//criar os 'buttons' '+,-,*,/,x²,pow,sqrt,root,Log,Ln,+-,.,(,)=,CL,DEL,R' .
+//criar os 'buttons' '+,-,*,/,x²,pow,sqrt,root,Log,Ln,+-,.,(,),=,CL,DEL,R' .
 div.innerHTML = div.innerHTML+'<button class="operator1" >'+'+'+'</button>'+'<button class="operator1">'+'-'+
 '</button>';
 div.innerHTML = div.innerHTML + '<button class="operator1">'+'*'+'</button>'+'<button class="operator1">'+'/'+
@@ -32,82 +39,72 @@ div.innerHTML = div.innerHTML +'<button class="operator1">'+'.'+'</button>'+
 '<button onclick=equalSign()>'+'='+'</button>'+'<button  onclick="clearAll()">'+'CL'+'</button>'+
 '<button  onclick="delAll()">'+'DEL'+'</button>'+'<button  onclick="window.location.reload()">'+'R'+'</button>';
 
-// obter todos os 'buttons' .
+console.log(div);
 const buttons = document.querySelectorAll('button');
-//obter os 'buttons' de 0 a 9 .
-const elements = document.querySelectorAll('.number');
-//obter os 'buttons' '+,-,*,/' .
+//console.log('buttons: ');
+//console.log(buttons);
+//obter os 'buttons' de 0 a 9 e insere na lista elements .
+const elements = document.querySelectorAll('.number');//'elements': Lista do tipo NodeList(Não é array) com todos buttons de 0 a 9 .
+//obter os 'buttons' '+,-,*,/' e insere na lista operator1 .
 const operator1 = document.querySelectorAll('.operator1');
+
+//console.log(elements);
+//console.log(elements[0]);//<button class="number">0</button>
+//console.log(elements[0].nextElementSibling);//<button class="number">1</button>
+//console.log(operator1);
+//console.log(operator1[0].innerText);
 
 //adicionar eventos aos 'buttons' de 0 a 9 .
 elements.forEach( (item) => {
-    item.addEventListener('click',() => {       
-        input.value = input.value + item.innerText;     
-    }); 
-});
-
-//adicionar eventos aos 'buttons' '+,-,*,/' e obtém o primeiro operando .
-operator1.forEach( (item) => {
-    item.addEventListener('click',() => {        
+    item.addEventListener('click',() => {
+        //array.push(item.innerText);
         input.value = input.value + item.innerText;
+        //console.log(array);
     });
+    
+   // console.log(item);//<button class="number">0</button>
+    //console.log(item.innerText);
 });
 
-//potência de 2 .
-function pow2() {
-    if(validation2()){
-        if(input.value == 0) return input.value = 0;
-        let num=Number((Math.pow(Number(input.value),2)).toFixed(DECIMAL));
-        array.length=0;//para caso o 'button =' for pressionado .
-        if (num==Infinity){
-            input.value = 'OverLoad'.toUpperCase();
-            lock();
-            return false;       
-        }else {
-            input.value = num;
-            return true;
-        }
-    } 
-}
-
-//potência qualquer .
-function pow() {    
-    array.length=0;//para caso o 'button =' for pressionado .
-    array.push(input.value+'**');
-    input.value=input.value+'^';    
-}
-
-// raiz quadrada
-function squareRoot() {  
-    if(input.value < 0) return input.value = 'There is no!'  
-    if(input.value == 0) return input.value = 0;
-    input.value = (Math.sqrt(Number(input.value))).toFixed(10);
-}
-
-// raiz de índice qualquer
-function root() {    
-   A = input.value;
-   input.value = input.value + 'r';
-}
+//adicionar eventos aos 'buttons' '+,-,*,/,.,(,)' .
+operator1.forEach( (item) => {
+    item.addEventListener('click',() => {
+        //array.push(item.innerText);
+        //A=input.value; // primeiro operando .
+        input.value = input.value + item.innerText;
+        //console.log(array);
+    });
+  
+    //console.log(item);//<button class="operator1">+</button> 
+});
 
 //executa a operação ao pressionar o 'button =' .
-function equalSign() {     
-    if(validation()) input.value = eval(A.join('')); 
-    A.length=0;   
+function equalSign() {  
+    console.log('input: '+input.value);
+    console.log('A= '+A);
+    if(validation()) input.value = (eval(A.join(''))).toFixed(DECIMAL); 
+    A.length=0;
+    console.log('A= '+A);
 }
 
+// validar a expressão entrada .
+// saída é a variável A,usada para o cálculo .
 function validation() {
-    const permit=['0','1','2','3','4','5','6','7','8','9','.','*','/','+','-','^','sqrt',
-                  'radix','e','E','(',')'];
+    const permit=['0','1','2','3','4','5','6','7','8','9','.','*','/','+','-','^','sqrt','radix',
+                  '(',')','[',']','{','}','e','E'];
     const algarism=['0','1','2','3','4','5','6','7','8','9'];
-
-    if (input.value.length < 3) {
+    const especials=['(',')','[',']','{','}'];
+    let begin = input.value.charAt(0);
+    console.log(input.value.charAt(0));
+    // string de entrada com tamanho menor que três ou início da string não é um número .
+    if ( (input.value.length < 3) || ( !(algarism.includes(begin)) && !(especials.includes(begin)) ) ) {       
         nAn();
-        return false;
-    }    
+        return false;            
+    }
+
     for(let i of input.value) {
         if(permit.includes(i)) { // verificar se cada caracter é permitido .
-            if (A.includes(i)) { // verificar se existe caracter é repetido,exceto número .
+            if (A.includes(i)) { // verificar se existe caracter repetido,exceto número .
                 if(!(algarism.includes(i))) {
                     nAn();
                     return false;
@@ -124,6 +121,90 @@ function validation() {
     return true;// passou
 }
 
+//potência de 2 .
+function pow2() {
+    console.log(A);
+    if(input.value == 0) return input.value = 0;
+    if(validationPow2()) {        
+        let num=(Number((Math.pow(Number(A.join('')),2)))).toFixed(DECIMAL);
+        A.length=0;//para a próxima operação,e caso o 'button =' for pressionado .
+        if (num==Infinity){
+            input.value = 'OverLoad'.toUpperCase();
+            lock();
+            return false;       
+        }else {
+            input.value = num;
+            return true;
+        }
+    } 
+}
+//potência qualquer .
+function pow() {
+    //if(typeof(input.value) != 'number') return input.value = 'Not a number!';
+    //A = input.value; // obtém o primeiro operando .
+    //console.log('A= '+A);
+    array.length=0;//para caso o 'button =' for pressionado .
+    array.push(input.value+'**');
+    input.value=input.value+'^';
+    // = input.value + '^';
+    //console.log('A-> '+A); 
+   //console.log(input.value);
+    //input.value = input.value+'pow';
+}
+// validar a expressão entrada para pow2 .
+// saída é a variável A,usada para o cálculo .
+function validationPow2() {
+    console.log(input.value);       
+    let permit=['0','1','2','3','4','5','6','7','8','9','.','-','e','E','+']; 
+    const algarism=['0','1','2','3','4','5','6','7','8','9'];
+    if(!(algarism.includes(input.value.charAt(0)))){// verificar se o primeiro caracter é número .
+        nAn();
+        return false;
+    }
+   // A=[...input.value];
+   // console.log(A);
+    for(let i of input.value){
+        if(!((i == 'e' || i == 'E') && (input.value.length>2))){ //entrada '2e' ou '2e'
+             nAn();
+            return false;
+        }
+    }
+    for(let i of input.value) {
+        if(permit.includes(i)) { // verificar se cada caracter é permitido .
+            if (A.includes(i)) { // verificar se existe caracter repetido,exceto número .
+                if(!(algarism.includes(i))) {
+                    nAn();
+                    return false;
+                }
+            }
+            A.push(i);// entrada filtrada,e será usada para o cálculo .
+            console.log(A);
+
+        }else{
+            nAn();
+            return false; 
+        }                   
+    }
+    console.log('A= '+A);
+    return true;// passou
+}
+
+function squareRoot() {
+   // if(typeof(input.value) != 'number') return input.value = 'Not a number!';
+    if(input.value < 0) return input.value = 'There is no!'  
+    if(input.value == 0) return input.value = 0;
+    input.value = (Math.sqrt(Number(input.value))).toFixed(10);
+}
+
+function root() {
+   //if(validation()) return input.value = 'Not a number!';   
+   A = input.value;
+   input.value = input.value + 'r';  
+
+}
+
+
+
 function nAn() {
     input.value = 'not a number'.toUpperCase();
     lock();
@@ -134,35 +215,15 @@ function lock() {
     input.style.color = "green";
     input.style.fontweight ="bold";
     input.disabled = true;
-    buttons.forEach((item)=>{ //desabilitar os 'buttons' exceto o 'button R' .       
-        if(!(item.innerText == 'R')) item.disabled= true;      
+    buttons.forEach((item)=>{ //desabilitar os 'buttons' exceto o 'button R' .
+        //console.log(item);
+        if(!(item.innerText == 'R')) item.disabled= true;        
+        //console.log(item);
     });
 }
 
 function clearAll() {
    // console.log("clearAll");
     input.value="";
-    array.length=0;   
+    A.length=0;   
 }
-
-// validar a expressão entrada para pow2 .
-function validation2() {
-    console.log(input.value);
-    let q=console.log(Number(input.value));
-    if(q == NaN) return console.log('not a number');
-    let permit=['0','1','2','3','4','5','6','7','8','9','.','-']; 
-    let ar=[...input.value];
-
-    console.log(ar);
-    let filter=ar.filter(element => element === 'e' || element === 'e'.toUpperCase() || element === '+');
-    console.log(filter); 
-    for(let i of input.value) {
-        if(!permit.includes(i)) { 
-            if(!filter.includes(i)){
-                 
-            }
-        } 
-    }
-
-    return true;
-} 
