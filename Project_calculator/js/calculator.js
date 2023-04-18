16ff8b19f73320aea8e7fd1b433c7cb90d5a25c4
javabin7777777 <fisica14@hotmail.com>
2023-04-18 16:27:43 -0300

    update calculator.js e calculator.css

--- Project_calculator/js/calculator.js
+++ Project_calculator/js/calculator.js
@@ -1,23 +1,27 @@
 "use strict";
 
 /* inicialização de variáveis */
-const DECIMAL=10;
+const DECIMAL=10;// usada para precisão do resultado de uma operação.
+//arrays usados para validação da string de entrada.
 const algarism=['0','1','2','3','4','5','6','7','8','9'];
 const operator=['*','/','+','-'];    
 const special_O=['(','[','{',];
 const special_C=[')',']','}'];
 const others=['.','e','E'];
 const permit=[...algarism,...operator,...special_O,...special_C,'.','sqrt','radix','e','E'];
-console.log(permit);
 const input = document.querySelector("#input");//campo texto .
 const div= document.querySelector("#containerButton");// div dos 'buttons'.
+let aux=""; // usada pela memory
+let callCount=0; // usada pela signMinus 
+let radix=false; // usada para pela root
 input.value="";
+
 //criar os 'buttons' de '0 a 9'
 for (let i = 0; i < 10; i++) {
     div.innerHTML = div.innerHTML + `<button class="number">${i}</button>`;
 }
 
-//criar os 'buttons' '+,-,*,/,x²,pow,sqrt,root,Log,Ln,+-,.,(,),=,CL,DEL,R'.
+//criar os 'buttons' '+,-,*,/,x²,pow,sqrt,root,Log,Ln,+-,.,(,),=,CL,DEL,MR,R'.
 div.innerHTML = div.innerHTML+'<button class="operator1" >'+'+'+'</button>'+'<button class="operator1">'+'-'+'</button>';
 div.innerHTML = div.innerHTML + '<button class="operator1">'+'*'+'</button>'+'<button class="operator1">'+'/'+'</button>';
 div.innerHTML = div.innerHTML + '<button  onclick="pow2()">'+'x²'+'</button>'+'<button onclick="pow()">'+'x<sup>y</sup>'+'</button>';
@@ -31,39 +35,73 @@ div.innerHTML = div.innerHTML + '<button  onclick="signMinus()">'+'+/-'+'</butto
 div.innerHTML = div.innerHTML +'<button class="operator1">'+'.'+'</button>'+'<button class="operator1">'+
 '('+'</button>'+'<button class="operator1">'+')'+'</button>'+'<button onclick=equalSign()>'+'='+'</button>'+
 '<button  onclick="clearAll()">'+'CL'+'</button>'+'<button  onclick="delAll()">'+'DEL'+'</button>'+
-'<button  onclick="window.location.reload()">'+'R'+'</button>';
+'<button onclick="memory()">'+'MR'+'</button>'+'<button  onclick="window.location.reload()">'+'R'+'</button>';
 
 const buttons = document.querySelectorAll('button');
 //obter os 'buttons' de 0 a 9 e insere na lista elements .
-const elements = document.querySelectorAll('.number');
+const elements = document.querySelectorAll('.number');//'elements': Lista do tipo NodeList(Não é array) com todos buttons de 0 a 9 .
 //obter os 'buttons' '+,-,*,/,.,(,)' e insere na lista operators .
 const operators = document.querySelectorAll('.operator1');
 const combined = [...elements,...operators];
+
 //adicionar eventos aos 'buttons' de 0 a 9 e aos 'buttons' '+,-,*,/,.,(,)' .
 combined.forEach( (item) => {
-    item.addEventListener('click',() => {       
-        input.value = input.value + item.innerText;       
-    });
+    item.addEventListener('click',() => {
+        input.value = input.value + item.innerText;      
+    }); 
 });
 
 //executa a operação ao pressionar o 'button =' .
-function equalSign() {     
-    if(validation()) {
+function equalSign() { 
+    let str2="";// índice.
+    let str1="";// radicando.
+    aux=input.value;  
+    if(validation()) {  
+        if(radix) { // raiz de índice(número) qualquer de um radicando(número) qualquer.Relacionado com a funçãp root.
+            for (let index = 0; index < input.value.length; index++) {
+                if(input.value[index]=='*' && input.value[index+1]=='*') {
+                    str1=input.value.substring(0,index);
+                    str2=input.value.substring(index+2,input.value.length);
+                    break;                                                                            
+                }               
+            }           
+            // validação da string de entrada.
+            let number=(Number(str2))-(Math.trunc(Number(str2)));           
+            let a=Number(str1)>0;
+            let b=Number(str2)>=2;
+            let c=Number(str2)%2==0;
+            let d=number==0;
+            if(!((a&&b&&d) || (b&&(!c)&&d))) { 
+                input.value='There is no'.toUpperCase();
+                lock();
+                return false;
+            }
+            if((!a) && (!c)) { // índice ímpar e radicando negativo.
+                input.value=(-1)*( Number(((Math.abs(Number(str1)))**(1/Number(str2))).toFixed(DECIMAL)) );                
+            }else {
+                input.value=Number( (Number(str1)**(1/(Number(str2)))).toFixed(DECIMAL) );// para outros casos.
+            }           
+            radix=false;
+            return;
+        }
+
         let number = Number(eval(input.value).toFixed(DECIMAL));
         if (number==Infinity) {
             input.value = 'OverLoad'.toUpperCase();
             lock();
             return false;       
         }else {
-            input.value = number;
+            input.value = number;           
             return true;        
         }
+        
     }else nAn();   
 }
 
 // validar a string de entrada.
-function validation() { 
-   // início da verificação preliminar da string de entrada .   
+function validation() {   
+   // início da verificação preliminar da string de entrada .
+    console.log(input.value);
     let counter_O=0;
     let counter_C=0;
     let number=true;
@@ -81,7 +119,7 @@ function validation() {
         let a=input.value.length>2;
         let b=algarism.includes(begin) || special_O.includes(begin) || begin == '-';
         let c=algarism.includes(end) || special_C.includes(end);       
-        let d;let e;let f;let g;let h;let j;let k;let before;let after;
+        let d;let e;let f;let g;let h;let j;let k;let before;let after; 
 
         // string de entrada com tamanho maior que dois.
         // iniciar com número ou com um dos caracteres 'special_O'.
@@ -125,33 +163,24 @@ function validation() {
                 // caso input.value[i] seja operador:depois dele pode ser número ou special aberto,exceto para o operador '**'.
                 before = a || d;
                 after =  e || g;
-                if(operator.includes(input.value[i])) {
-                    console.log('before: '+before);
-                    console.log('after: '+after);
+                if(operator.includes(input.value[i])) {                   
                     if(!(before && after)) {
                         if(input.value[i+1]=='*') i+=1;//caso seja os caracteres '**'.
-                        else return false;
-                        console.log('passou aqui');
+                        else return false;                       
                     }
                 }
 
                 // caso input.value[i] seja caracter special_O: antes dele pode ser operador e depois pode ser número.
                 before =  b;
                 after =  e;
-                if(special_O.includes(input.value[i])) {
-                    console.log('before: '+before);
-                    console.log('after: '+after); 
-                    if(!(before && after)) return false;
-                    console.log('passou aqui');
+                if(special_O.includes(input.value[i])) {                  
+                    if(!(before && after)) return false;                  
                 }
                 // caso input.value[i] seja caracter special_C: antes dele pode ser número e depois pode ser operador.
                 before =  a ;
                 after =  f;
                 if(special_C.includes(input.value[i])) { 
-                    console.log('before: '+before);
-                    console.log('after: '+after);
                     if(!(before && after)) return false;
-                    console.log('passou aqui');
                 }
             }else {
                 // último caracter da string de entrada.
@@ -163,7 +192,8 @@ function validation() {
 }
 
 //potência de 2 .
-function pow2() {   
+function pow2() { 
+    aux=input.value;  
     if(input.value == 0) return input.value = 0;
     if(validationPow()) { // validação da entrada .      
         let num=Number((Number(input.value)**2).toFixed(DECIMAL));        
@@ -172,7 +202,7 @@ function pow2() {
             lock();
             return false;       
         }else {
-            input.value = num;
+            input.value = num;           
             return true;
         }
     } 
@@ -184,7 +214,6 @@ function pow() {
 }
 
 function validationPow() {
-    console.log(input.value);
     const permitPow=['0','1','2','3','4','5','6','7','8','9','.','-','e','E','+'];
     if(input.value.length==0){
         nAn();
@@ -225,33 +254,58 @@ function nAn() {
     lock();
 }
 
-//desabilitar o campo texto e os 'buttons',exceto 'button R' .
+//desabilitar o campo texto e os 'buttons',exceto 'button R' e 'button MR' .
 function lock() {
     input.style.color = "green";
     input.style.fontweight ="bold";
     input.disabled = true;
-    buttons.forEach((item)=>{         
-        if(!(item.innerText == 'R')) item.disabled= true; 
+    buttons.forEach((item)=> {         
+        if(!(item.innerText == 'R' || item.innerText == 'MR')) item.disabled= true;
     });
 }
 
 function squareRoot() {  
-    if(Number(input.value) < 0) return input.value = 'There is no'  
+    if(Number(input.value) < 0) {
+        input.value = 'There is no'.toUpperCase();
+        lock();
+        return false;  
+    } 
     if(Number(input.value) == 0) return input.value = 0;
-    input.value = Number( (Number(input.value)**(1/2)).toFixed(DECIMAL) );
+    input.value = Number((Number(input.value)**(1/2)).toFixed(DECIMAL));
 }
 
 function root() {     
-   input.value = input.value + 'r';
+    input.value = input.value+'**';
+    radix=true;
 }
 
 function clearAll() {  
-    input.value="";     
+    input.value="";
+    aux=""; 
+    callCount=0;
+    radix=false;  
 }
 
-function  delAll() {
+function delAll() {
     let array=[...input.value];
     array.pop();
-    input.value=array.join('');
-    if(input.value.length==0) clearAll();   
+    input.value=array.join('');   
+}
+ 
+function memory() {
+    input.disabled=false;   
+    input.value=aux;
+}
+
+function signMinus() { 
+   let arr=[...input.value];   
+    if(callCount%2==0) {
+        arr.unshift('-');
+        input.value=arr.join('');
+    }
+    else {
+        arr.shift();
+        input.value=arr.join('');
+    }
+   ++callCount;
 }
