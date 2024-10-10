383c0b564b2a5642bbb61cd2dd2fd2bf5cf8cd26
javabin7777777 <fisica14@hotmail.com>
2024-10-10 18:14:41 -0300

    Atualização do projeto conversor de moedas

--- projetosJava/com.samzubeli.ConversorDeMoedas/src/com/samzubeli/ConversorDeMoedas/api/Conexao.java
+++ projetosJava/com.samzubeli.ConversorDeMoedas/src/com/samzubeli/ConversorDeMoedas/api/Conexao.java
@@ -15,30 +15,31 @@ public class Conexao {
 			url = url.concat(key);
 			HttpClient client = HttpClient.newHttpClient();
 			HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
-			// Envio da requisição e obter a resposta.
+			// Envio da requisição e obter a resposta. 
 			HttpResponse<String> resposta = client.send(request, BodyHandlers.ofString());
-
+			
 			return resposta.body();
-
+			
 		} catch (IOException | InterruptedException e) {
 			System.out.println("Houve um erro de comunicacao com a API WEB");
 			System.out.println(e.getMessage());
 			throw new RuntimeException(e);
 		}
-
+		
 	}
 }
 
 /*
  * https://v6.exchangerate-api.com/v6/YOUR-API-KEY/pair/EUR/GBP -> devolve no
- * json a taxa de câmbio.
+ * json a taxa de câmbio
  * 
- * EUR -> moeda base. USD -> moeda alvo. "YOUR-API-KEY/pair/EUR/GBP" ou
- * "YOUR-API-KEY/pair/EUR/GBP/AMOUNT"
+ * EUR -> moeda base USD -> moeda alvo key =
+ * "6f7d444c2c79803d656753f8/pair/EUR/GBP" ou
+ * "6f7d444c2c79803d656753f8/pair/EUR/GBP/AMOUNT"
  * 
  * GET https://v6.exchangerate-api.com/v6/YOUR-API-KEY/pair/EUR/GBP/AMOUNT ->
  * devolve no json moeda alvo já convertida.
  * 
- * Ex: https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/USD -> devolve
- * todas moedas convertidas em relação ao dollar.
+ * Ex: https://v6.exchangerate-api.com/v6/6f7d444c2c79803d656753f8/latest/USD ->
+ * devolve todas moedas convertidas em relação ao dollar.
  */
